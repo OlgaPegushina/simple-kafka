be5bc32e2fb6bbc5c656ccb9bf93164ae82a98dd
OlgaPegushina <pegushina.shine@yandex.ru>
2025-07-19 19:32:30 +0300

    Пример отправки и получения строк

---
+++ src/main/java/ru/practicum/KafkaConsumerExample.java
@@ -0,0 +1,33 @@
+package ru.practicum;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class KafkaConsumerExample {
+
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.VoidDeserializer");
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-service-1");
+
+        try (Consumer<Void, String> consumer = new KafkaConsumer<>(config)) {
+            consumer.subscribe(List.of("example-topic", "weather-events"));
+
+            while (true) {
+                ConsumerRecords<Void, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<Void, String> record : records) {
+                    System.out.printf("topic = %s, offset = %d, value = %s%n", record.topic(), record.offset(), record.value());
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/practicum/KafkaProducerExample.java
@@ -0,0 +1,28 @@
+package ru.practicum;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class KafkaProducerExample {
+
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.VoidSerializer");
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<Void, String> producer = new KafkaProducer<>(config);
+
+        String message = "Снова привет, Kafka!";
+        String topic = "example-topic";
+        ProducerRecord<Void, String> record = new ProducerRecord<>(topic, message);
+
+        producer.send(record);
+
+        producer.close();
+    }
+}
